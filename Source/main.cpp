a0f58e37415e4b01c3f7086eed3015ed2f059d44
Yang Kyowon <seolyang.contact@gmail.com>
2024-03-19 12:49:18 +0900

    Convert to embed!

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,2 +1,75 @@
 #include <serie.h>
-#include <ranges>
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv)
+{
+    using namespace serie;
+    CLI::App app{ "Embed blob to header like file" };
+
+    std::vector<fs::path> targetPaths;
+    app.add_option("-f,--filename", targetPaths, "File paths to embed")
+        ->expected(1, 128)
+        ->required();
+
+    uint8_t newlineInterval = 0;
+    app.add_option("-n,--newline", newlineInterval, "Newline interval")
+        ->default_val(8)
+        ->expected(1, 32);
+
+    CLI11_PARSE(app, argc, argv);
+
+    for (const fs::path& targetPath : targetPaths)
+    {
+        fs::path outputPath{ targetPath };
+        outputPath.replace_extension(serie::EmbededFileExtension);
+        std::cout << std::format("Embed blob {} to {}.\n", targetPath.string(), outputPath.string());
+
+        if (!fs::exists(targetPath))
+        {
+            std::cerr << "Target path does not exist." << std::endl;
+            SERIE_NOENTRY();
+            continue;
+        }
+
+        const size_t sizeOfBlob = fs::file_size(targetPath);
+        std::ifstream inputStream{ targetPath, std::ios::binary | std::ios::in };
+        if (!inputStream.is_open())
+        {
+            std::cerr << "Failed to open input stream. " << targetPath << std::endl;
+            SERIE_NOENTRY();
+            continue;
+        }
+
+        if (sizeOfBlob == 0)
+        {
+            std::cerr << "The size of blob must greater than 0. " << targetPath << std::endl;
+            SERIE_NOENTRY();
+            continue;
+        }
+
+        std::vector<uint8_t> blob(sizeOfBlob);
+        SERIE_CHECK(blob.size() == sizeOfBlob);
+        inputStream.read((char*)blob.data(), blob.size());
+        inputStream.close();
+
+        const std::string embedName = targetPath.filename().replace_extension().string();
+        std::optional<std::string> embededBlob = EmbedBlob(blob, embedName, newlineInterval);
+        if (!embededBlob)
+        {
+            std::cerr << "Failed to embed blob." << targetPath << std::endl;
+            continue;
+        }
+
+        std::ofstream outputStream{ outputPath };
+        if (!outputStream.is_open())
+        {
+            std::cerr << "Failed to open output stream." << outputPath << std::endl;
+            SERIE_NOENTRY();
+            continue;
+        }
+        outputStream << *embededBlob;
+        outputStream.close();
+    }
+
+    return 0;
+}
--- Source/serie.h
+++ Source/serie.h
@@ -8,19 +8,34 @@
 #include <sstream>
 #include <format>
 
+#if (defined(_DEBUG) || defined(DEBUG))
+#define SERIE_CHECK(EXPRESSION) \
+        do                        \
+            if (!(EXPRESSION))    \
+            {                     \
+                __debugbreak();   \
+            }                     \
+        while (false)
+#else
+#define SERIE_CHECK(CONDITION) ((void)0)
+#endif
+
+#define SERIE_NOENTRY() SERIE_CHECK(false)
+
 namespace serie
 {
     constexpr std::string_view EmbededFileExtension = ".embed";
     namespace fs = std::filesystem;
 
-    static std::optional<std::string> BlobToEmbed(const std::span<const uint8_t> blob, const std::string_view embedName = "gEmbedBlob", const uint8_t newlinePerBytes = 8)
+    static std::optional<std::string> EmbedBlob(const std::span<const uint8_t> blob, const std::string_view embedName = "gEmbedBlob", const uint8_t newlinePerBytes = 8)
     {
-        if (blob.data() == 0)
+        if (blob.empty())
         {
+            SERIE_NOENTRY();
             return std::nullopt;
         }
 
-        constexpr std::string_view BaseEmbedingFormat = "constexpr inline uint8_t {}[] = {{ {} }};";
+        constexpr std::string_view BaseEmbedingFormat = "constexpr inline uint8_t {}[] = \n{{\n{}\n}};";
 
         std::stringstream stream;
         uint8_t counter = 0;
@@ -32,7 +47,7 @@ namespace serie
                 counter = 0;
             }
 
-            stream << std::format("{}, ", byte);
+            stream << std::format("{:#x}, ", byte);
             ++counter;
         }
 
